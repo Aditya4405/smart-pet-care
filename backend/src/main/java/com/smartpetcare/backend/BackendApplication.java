5b308870397bfbe9816ff16c627862d793065bb8
Aditya Prajapati <136999401+Aditya4405@users.noreply.github.com>
2026-03-15 01:33:42 +0530

    update in Main Application

--- backend/src/main/java/com/smartpetcare/backend/BackendApplication.java
+++ backend/src/main/java/com/smartpetcare/backend/BackendApplication.java
@@ -2,14 +2,12 @@ package com.smartpetcare.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.EnableScheduling; 
 
 @SpringBootApplication
 @EnableScheduling
 public class BackendApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
