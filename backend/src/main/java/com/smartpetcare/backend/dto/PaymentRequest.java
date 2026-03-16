c5ceaf6c000d4977690e221c79cceee8ca46fa75
Aditya Prajapati <136999401+Aditya4405@users.noreply.github.com>
2026-03-16 10:41:26 +0530

    Add Payment Request

---
+++ backend/src/main/java/com/smartpetcare/backend/dto/PaymentRequest.java
@@ -0,0 +1,36 @@
+package com.smartpetcare.backend.dto;
+
+import lombok.Data;
+
+@Data
+public class PaymentRequest {
+    private String razorpay_order_id;
+    private String razorpay_payment_id;
+    private String razorpay_signature;
+    private Long appointmentId; // For appointments
+	public String getRazorpay_order_id() {
+		return razorpay_order_id;
+	}
+	public void setRazorpay_order_id(String razorpay_order_id) {
+		this.razorpay_order_id = razorpay_order_id;
+	}
+	public String getRazorpay_payment_id() {
+		return razorpay_payment_id;
+	}
+	public void setRazorpay_payment_id(String razorpay_payment_id) {
+		this.razorpay_payment_id = razorpay_payment_id;
+	}
+	public String getRazorpay_signature() {
+		return razorpay_signature;
+	}
+	public void setRazorpay_signature(String razorpay_signature) {
+		this.razorpay_signature = razorpay_signature;
+	}
+	public Long getAppointmentId() {
+		return appointmentId;
+	}
+	public void setAppointmentId(Long appointmentId) {
+		this.appointmentId = appointmentId;
+	}
+    
+}
