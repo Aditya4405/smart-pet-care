acc8027d10c0b7eece251f749a09d79105b9b576
Aditya Prajapati <136999401+Aditya4405@users.noreply.github.com>
2026-03-18 21:22:45 +0530

    Update AuditLog Repositories

---
+++ backend/src/main/java/com/smartpetcare/backend/repository/AuditLogRepository.java
@@ -0,0 +1,8 @@
+package com.smartpetcare.backend.repository;
+
+import com.smartpetcare.backend.entity.AuditLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AuditLogRepository extends JpaRepository<AuditLog, Long> {}
